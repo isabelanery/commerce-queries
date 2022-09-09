2941db9aff5b1881d3598d1ca43d0d1e13c42965
Isabela Nery <isabelanery.r@gmail.com>
2022-09-09 10:24:16 -0300

    feat: lint again

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,6 +1,10 @@
 db.produtos.find(
-  { $and: [
-    { vendidos: { $ne: 50} },
-    { tags: { $exists: 0 } }
-  ] },
-  { _id: 0, nome: 1, vendidos: 1 });
+  {
+    $and:
+    [
+      { vendidos: { $ne: 50} },
+      { tags: { $exists: 0 } },
+    ]
+  },
+  { _id: 0, nome: 1, vendidos: 1 }
+);
--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,7 +1,13 @@
 db.produtos.find(
-  { valoresNutricionais: 
-    { $elemMatch: 
-      { tipo: 'calorias', 
-        quantidade: { $lt: 500 } }
-    }},
-    { _id: 0, nome: 1 } );
+  { 
+    valoresNutricionais:
+    { 
+      $elemMatch:
+      {
+        tipo: 'calorias',
+        quantidade: { $lt: 500 },
+      }
+    } 
+  },
+  { _id: 0, nome: 1 }
+);
