7e9b6f206adf1c11ccc5364c4e7b032d5aac60e3
Isabela Nery <isabelanery.r@gmail.com>
2022-09-12 19:27:04 -0300

    feat: query 17

--- challenges/desafio16.js
+++ challenges/desafio16.js
@@ -1,2 +1,6 @@
-db.produtos.updateOne({ nome: "Big Mac" }, { $currentDate: { ultimaModificacao: true } })
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  { $currentDate: { ultimaModificacao: true } },
+);
+
 db.produtos.find({ ultimaModificacao: { $exists: 1 } }, { _id: 0, nome: 1 });
